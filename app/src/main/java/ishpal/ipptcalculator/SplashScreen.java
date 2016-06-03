fe0e519b05d2dded84c6603d2dd311c44691b1ef
ishpal26 <ishpalsinghgill@live.co.uk>
2016-06-03 13:09:07 +0800

    Added splash screen during launch

---
+++ app/src/main/java/ishpal/ipptcalculator/SplashScreen.java
@@ -0,0 +1,39 @@
+package ishpal.ipptcalculator;
+
+/**
+ * Created by Ishpal on 3/6/2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
