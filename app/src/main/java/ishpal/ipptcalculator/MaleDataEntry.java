4ee3552c85d3e88967e017b201d5dba082938a8f
Lutfan <lutfan@gmail.com>
2016-06-16 01:16:33 +0800

    Changed the button

--- app/src/main/java/ishpal/ipptcalculator/MaleDataEntry.java
+++ app/src/main/java/ishpal/ipptcalculator/MaleDataEntry.java
@@ -1,43 +1,60 @@
 package ishpal.ipptcalculator;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
+//import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MaleDataEntry extends AppCompatActivity implements View.OnClickListener {
     private Spinner spin;
     EditText minutes,seconds,pushUps,sitUps;
-    Button calculate;
+    //Button calculate;
     int givenAge = 0;
-    ArrayAdapter<CharSequence> adapter;
+    //ArrayAdapter<CharSequence> adapter;
+    TextView t2, t3, t4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_male_data_entry);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         Intent intentExtras = getIntent();
         Bundle extraBundle = intentExtras.getExtras();
+
+        t2 =  (TextView) findViewById(R.id.textView2);
+        Typeface myCustomFont2 = Typeface.createFromAsset(getAssets(), "Fonts/Roboto-Thin.ttf");
+        t2.setTypeface(myCustomFont2);
+
+        t3 =  (TextView) findViewById(R.id.textView3);
+        Typeface myCustomFont3 = Typeface.createFromAsset(getAssets(), "Fonts/Roboto-Thin.ttf");
+        t3.setTypeface(myCustomFont3);
+
+        t4 =  (TextView) findViewById(R.id.textView4);
+        Typeface myCustomFont4 = Typeface.createFromAsset(getAssets(), "Fonts/Roboto-Thin.ttf");
+        t4.setTypeface(myCustomFont4);
 
         if (extraBundle.isEmpty() == true){
         } else {
             givenAge = extraBundle.getInt("age");
             //just to check
-            Toast.makeText(this,"age is: "+givenAge, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Age: "+givenAge, Toast.LENGTH_SHORT).show();
         }
 
-        calculate = (Button) findViewById(R.id.calButton);
-        calculate.setOnClickListener(this);
+        /*calculate = (Button) findViewById(R.id.calButton);
+        calculate.setOnClickListener(this);*/
         minutes = (EditText) findViewById(R.id.mins);
         seconds = (EditText) findViewById(R.id.secs);
         pushUps = (EditText) findViewById(R.id.pushUpReps);
@@ -58,20 +75,21 @@ public class MaleDataEntry extends AppCompatActivity implements View.OnClickList
 
             }
         }); */
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
     }
 
     @Override
     public void onClick(View v){
         switch (v.getId()) {
-            case R.id.calButton:
+            case R.id.fab:
                 int runMin = Integer.parseInt(minutes.getText().toString().trim());
                 int runSec = Integer.parseInt(seconds.getText().toString().trim());
                 int pushReps = Integer.parseInt(pushUps.getText().toString().trim());
--- app/src/main/java/ishpal/ipptcalculator/SplashScreen.java
+++ app/src/main/java/ishpal/ipptcalculator/SplashScreen.java
@@ -6,19 +6,28 @@ package ishpal.ipptcalculator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
+
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
+        //This portion makes the fonts nice
+        t =  (TextView) findViewById(R.id.header);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "Fonts/Roboto-Thin.ttf");
+        t.setTypeface(myCustomFont);
+
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(1000);
+                    sleep(3000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
